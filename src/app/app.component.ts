c217053d6587c42bd83dad471c80ab2506c67c85
Abdessadek <abdessadekhm@gmail.com>
2025-04-13 14:40:47 +0100

    current

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,22 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from "./home/home.component";
+import { ThemeService } from './services/theme.service';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'shifaa';
+  isDarkMode$: Observable<boolean>;
+
+  constructor(private themeService: ThemeService) {
+    this.isDarkMode$ = this.themeService.isDarkMode$;
+  }
 }
